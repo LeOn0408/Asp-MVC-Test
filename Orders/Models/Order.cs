f58e22bc0232e855d60b08f156d9404d5eb042ea
Александр Паршуков <20384838+LeOn0408@users.noreply.github.com>
2023-03-26 19:36:59 +0300

    Добавьте файлы проекта.

---
+++ Orders/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Orders.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        [Index("IX_Order_Number_ProviderId", 1, IsUnique = true)]
+        public string Number { get; set; }
+        public DateTime Date { get; set; }
+        [Index("IX_Order_Number_ProviderId", 2, IsUnique = true)]
+        public int ProviderId { get; set; }
+        public Provider Provider { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
---
+++ Orders/Models/Provider.cs
@@ -0,0 +1,9 @@
+﻿namespace Orders.Models
+{
+    public class Provider
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
